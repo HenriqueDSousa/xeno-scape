50a9f024992f68798b3f67fd45f426811dfa8329
Henrique D. <henriquedsousa122@gmail.com>
2025-11-27 21:33:21 -0300

    Add game tilte to main menu

--- Source/UI/Screens/MainMenu.cpp
+++ Source/UI/Screens/MainMenu.cpp
@@ -8,7 +8,13 @@
 MainMenu::MainMenu(Game* game, const std::string& fontName)
 : UIScreen(game, fontName) {
   mSize = Vector2(Game::WINDOW_WIDTH, Game::WINDOW_HEIGHT);
-  AddButton("Start Game", Vector2(GetSize().x / 2, GetSize().y / 3),
+
+  AddText("Xeno Scape", Vector2(2 * GetSize().x / 4, GetSize().y / 4),
+          Vector2::Zero,
+          40,
+          Color::White);
+
+  AddButton("Start Game", Vector2(2 * GetSize().x / 4, GetSize().y / 2),
           Vector2(GetSize().x / 4, 50.f),
           24,
           UIButton::TextPos::Center,
@@ -17,7 +23,7 @@ MainMenu::MainMenu(Game* game, const std::string& fontName)
           },
           Vector2::Zero,
           Color::White);
-  AddButton("Quit", Vector2(GetSize().x / 2, 2 * GetSize().y / 3),
+  AddButton("Quit", Vector2(GetSize().x / 2, GetSize().y / 2 + 60.0f),
         Vector2(GetSize().x / 4, 50.f),
         24,
         UIButton::TextPos::Center,
